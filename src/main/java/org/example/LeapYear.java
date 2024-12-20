5937c037d2f2986ba101a61d6350a28f5f3fd31d
BhuvanGudi <gudimetlabhuvan@gmail.com>
2024-12-19 17:47:58 -0700

    Q2 Leap Year (comments)

--- src/main/java/org/example/LeapYear.java
+++ src/main/java/org/example/LeapYear.java
@@ -6,11 +6,23 @@ public class LeapYear {
     public static void main(String[] args) {
         System.out.println("Please enter the year between 0 and 10,000");
         System.out.println("Do not enter negative or alphabets");
+        /**
+         * To have an input from the user, we use scanner.
+         * */
         Scanner scanner = new Scanner(System.in);
+        /**
+         * Store the following user input.
+         * */
         int user_input = scanner.nextInt();
+        /**
+         * Check the following user input.
+         * */
         if(user_input < 0 || user_input > 10000){
             System.out.println("Invalid input");
         }else{
+            /**
+             * If user input satisfies the conditions, check if it is a leap year.
+             * */
             if(user_input % 4 == 0) {
                 System.out.print(user_input);
                 System.out.print(" is a leap year!");
