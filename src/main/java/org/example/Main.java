211eab2d78ff606f7ffde8aa3a84d1aafe27a09c
BhuvanGudi <gudimetlabhuvan@gmail.com>
2024-12-19 17:27:29 -0700

    Q1 comments and changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,29 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int max_num = 0;
+        /**
+         * Create new int array and push 3 numbers.
+         * */
         int[] numbers = new int[3];
         numbers[0] = 6;
         numbers[1] = 1;
         numbers[2] = 8;
-        max_num = numbers[0];
+        /**
+         * Set initial index as a max value.
+         * */
+        int max_num = numbers[0];
+        /**
+         * For loop to check every index and change the max value accordingly.
+         * */
         for(int i=0; i < numbers.length - 1; i++){
-            if(max_num > numbers[i+1]){
-            }else{
+            if(max_num > numbers[i+1]) {}
+            else{
                 max_num = numbers[i+1];
             }
         }
+        /**
+         * Print max_value at the end.
+         * */
         System.out.println(max_num);
     }
 }
