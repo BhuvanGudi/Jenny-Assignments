e64d33b8ce2558868c366a143ba2819f93d6093e
BhuvanGudi <gudimetlabhuvan@gmail.com>
2024-12-19 17:44:31 -0700

    Q2 Leap Year (changes)

--- src/main/java/org/example/LeapYear.java
+++ src/main/java/org/example/LeapYear.java
@@ -7,14 +7,15 @@ public class LeapYear {
         System.out.println("Please enter the year between 0 and 10,000");
         System.out.println("Do not enter negative or alphabets");
         Scanner scanner = new Scanner(System.in);
-        if(scanner.nextInt() < 0 || scanner.nextInt() > 10000){
+        int user_input = scanner.nextInt();
+        if(user_input < 0 || user_input > 10000){
             System.out.println("Invalid input");
         }else{
-            if(scanner.nextInt() % 4 == 0) {
-                System.out.print(scanner.nextInt());
+            if(user_input % 4 == 0) {
+                System.out.print(user_input);
                 System.out.print(" is a leap year!");
             }else{
-                System.out.print(scanner.nextInt());
+                System.out.print(user_input);
                 System.out.print(" is not a leap year...");
             }
         }
